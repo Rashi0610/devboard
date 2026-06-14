4c18c2ba827de3e2dd8a3cbbd3e45701e75d280d
Rashi <rashishaha0610@gmail.com>
2026-06-14 19:20:31 +0530

    Enviornment variables for production

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -7,7 +7,7 @@ const getToken = () => {
     ?.split('=')[1]
 }
 
-const socket = io('http://localhost:5000', {
+const socket = io(import.meta.env.VITE_SERVER_URL || 'http://localhost:5000', {
   autoConnect: false,
   auth: {
     token: getToken()
