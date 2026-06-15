65eeb220d763bff4ec8b79c482f073ae983f9019
Rashi <rashishaha0610@gmail.com>
2026-06-15 14:38:16 +0530

    another frontend bug

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { GitBranch} from 'lucide-react'
 
 const Login = () => {
   const handleLogin = () => {
-    window.location.href = `${import.meta.env.VITE_API_URL}/api/auth/github`;
+    window.location.href = `${import.meta.env.VITE_API_URL}/auth/github`;
   }
 
   return (
