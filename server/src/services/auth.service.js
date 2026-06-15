c83fcb96ee4151b7a1264d759f64e8286960386c
Rashi <rashishaha0610@gmail.com>
2026-06-15 15:16:54 +0530

    Authentication bug fix

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -21,20 +21,20 @@ export const generateTokens = (user) =>{
 export const setTokenCookies = (res, accessToken, refreshToken) => {
     res.cookie('accessToken', accessToken, {
     httpOnly: true, 
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax', 
+    secure: true,
+    sameSite: 'none', 
     maxAge: 15 * 60 * 1000 
   })
   res.cookie('refreshToken', refreshToken, {
    httpOnly: true, 
-   secure: process.env.NODE_ENV === 'production', 
-   sameSite: 'lax', 
+   secure: true, 
+   sameSite: 'none', 
    maxAge: 7 * 24 * 60 * 60 * 1000 
   })
   res.cookie('socketToken', accessToken, {
   httpOnly: false,        // ← readable by JS, needed for socket auth
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
+  secure: true,
+  sameSite: 'none',
   maxAge: 15 * 60 * 1000
 })
 }
